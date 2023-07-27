c9ba8c38c4f6a4429006cfc89a1fe07a6aee3301
wiredmartian <solomzi.jikani@gmail.com>
2023-07-28 00:00:37 +0200

    create a typed run response

--- src/promise-queue.ts
+++ src/promise-queue.ts
@@ -1,3 +1,8 @@
+type PromiseBatchResult<D, E> = {
+    status: "rejected" | "fulfilled"
+} & { status: "fulfilled", data: D }
+    | { status: "rejected", error: E }
+
 export class PromiseQueue {
     private tasks: Array<Promise<any>>;
     private running: number;
@@ -9,21 +14,28 @@ export class PromiseQueue {
         this.running = 0;
     }
 
-    async run<T>() {
-        const result: T[] = []
+    async run<D, E>(): Promise<PromiseBatchResult<D, E>[]> {
+        const result: Array<PromiseBatchResult<D, E>> = []
+
         let maxTasks = Math.min(this.concurrent - this.running, this.tasks.length)
 
         while (maxTasks > 0) {
             const tasksBatch = this.tasks.splice(0, maxTasks)
             this.running += maxTasks
 
-            const batchResults = await Promise.allSettled<T>(tasksBatch)
+            const batchResults = await Promise.allSettled(tasksBatch)
 
             for (const batchResult of batchResults) {
                 if (batchResult.status === 'fulfilled') {
-                    result.push(batchResult.value)
+                    result.push({
+                        status: "fulfilled",
+                        data: batchResult.value,
+                    })
                 } else {
-                    result.push(batchResult.reason)
+                    result.push({
+                        status: "rejected",
+                        error: batchResult.reason
+                    })
                 }
                 this.running--
             }
